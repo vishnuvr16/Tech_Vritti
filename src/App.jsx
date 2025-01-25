d9e85386a90de6af7426c1d382bc09320acebde0
vishnuvr16 <vishnuvardhanreddygajjala1@gmail.com>
2025-01-25 18:39:55 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Homepage from "./pages/home"
+
+
+function App() {
+
+  return (
+    <div className="bg-gradient-to-br from-gray-900 to-blue-900 text-white min-h-screen">
+      <Homepage />
+    </div>
+  )
+}
+
+export default App
